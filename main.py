0086190d10a909e4612f8f7c40599312efd3d644
Yuna-Jetson <5911110149@mutacth.com>
2021-08-21 16:40:44 +0700

    done

--- main.py
+++ main.py
@@ -1,19 +1,45 @@
-import cv2
+import cv2 as cv
+from time import sleep, time
 
-vod = cv2.VideoCapture('./img/VID_milk.mp4')
 
-while True:
-    ret, frame = vod.read()
-    gpu_frame = cv2.cuda_GpuMat()
-
-    gpu_frame.upload(frame)
-    frame = cv2.cuda.resize(gpu_frame, (852,480))
-    frame.dowload()
-    ret,frame = vod.read()
+vod = cv.VideoCapture('./img/sea.mp4')
 
 # while True:
-#     _,img = vod.read()
-#     cv2.imshow('ss', img)
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#     start = time()
+#     _,frame = vod.read()
+#     frame = cv.resize(frame,(640,360),interpolation = cv.INTER_AREA)
+#     end = time()
+#     fps_label = "FPS: %.2f " % (1 / (end - start))
+#     cv.putText(frame, fps_label, (0, 45), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+#     cv.imshow('ss', frame)
+#     if cv.waitKey(1) & 0xFF == ord('q'):
 #         break
+
+
+
+ret, frame = vod.read()
+scale = 0.5
+gpu_frame = cv.cuda_GpuMat()
+
+while ret:
+    start = time()
+    gpu_frame.upload(frame)
+
+    resized = cv.cuda.resize(gpu_frame, (int(1280 * scale), int(720 * scale)))
+    resized = resized.download()
+
+    end = time()
+    fps_label = "FPS: %.2f " % (1 / (end - start))
+    cv.putText(resized, fps_label, (0, 45), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv.imshow('ss', resized)
+
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    ret, frame = vod.read()
+
+vod.release()
+cv.destroyAllWindows()
+
     
